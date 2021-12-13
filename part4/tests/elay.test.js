b00ba701d74e16f9317d963c97ae14985d72b1ab
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2021-12-13 12:50:45 +0200

    task 4.4 completed

--- part4/tests/elay.test.js
+++ part4/tests/elay.test.js
@@ -1,6 +1,28 @@
-const {dummy} = require('../utils/list_helper');
+const {dummy , totalLikes} = require('../utils/list_helper');
 
 test('testing dummy', ()=>{
     const array =[];
     expect(dummy(array)).toBe(1);
+})
+
+test('getting total likes', ()=>{
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 10,
+            __v: 0
+          }
+      ];
+    expect(totalLikes(listWithOneBlog)).toBe(15);
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,11 @@
 exports.dummy = (blogs)=>{
     return 1;
 }
+
+exports.totalLikes = (blogs)=>{
+    let likes = 0;
+    for (const blog of blogs) {
+       likes += blog.likes
+    }
+    return likes;
+}
